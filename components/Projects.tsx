3cb0d06189d53d46286f1efb1dc4fa0b844080fe
Artur Levchenko <99216307+LightForLife@users.noreply.github.com>
2023-06-27 23:26:18 +0300

    fix padding styles for project components

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -22,7 +22,7 @@ function Projects({ projects }: Props) {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center z-0"
+      className="h-screen relative flex overflow-hidden flex-col max-w-full justify-evenly text-center mx-auto items-center z-0"
     >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Projects
@@ -34,18 +34,19 @@ function Projects({ projects }: Props) {
           navigation={true}
           modules={[Navigation]}
           breakpoints={{ 700: { slidesPerView: 1 } }}
-          className="projectsSlider min-h-[600px]"
+          className="projectsSlider "
         >
           {projects.map((project, i) => (
             <SwiperSlide key={i}>
-              <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
+              <div className="w-screen text-center flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-[20px] md:p-44 h-screen">
                 <motion.img
                   initial={{ y: -300, opacity: 0 }}
                   transition={{ duration: 1.2 }}
                   whileInView={{ opacity: 1, y: 0 }}
                   viewport={{ once: true }}
-                  src={urlFor(project?.image).url()}
-                  alt=""
+                  src={urlFor(project?.image).width(800).url()}
+                  alt="A picture of the project on different devices"
+                  className=""
                 />
 
                 <div className="space-y-10 px-0 md:px-10 max-w-6xl">
@@ -58,19 +59,16 @@ function Projects({ projects }: Props) {
                   <div className="flex items-center space-x-2 justify-center">
                     {project?.technologies.map((technology) => (
                       <Image
-                        id="component"
                         className="h-10 w-10"
                         key={technology._id}
                         src={urlFor(technology.image).url()}
                         width={500}
                         height={500}
-                        alt="A picture of the project on different devices"
+                        alt="Pictures of technology logos"
                       />
                     ))}
                   </div>
-                  <p className="text-lg text-center md:text-left">
-                    {project.summary}
-                  </p>
+                  <p className="text-lg text-center">{project.summary}</p>
                 </div>
               </div>
             </SwiperSlide>
